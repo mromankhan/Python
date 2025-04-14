5c4ffb325a7dcd46dfeb98cc0f53c965fce58536
Muhammad Roman <mromankhan005@gmail.com>
2025-04-14 19:07:34 +0500

    Data Types

---
+++ Class_2/app.py
@@ -0,0 +1,66 @@
+# ----------------------------Strings----------------------------
+
+# Normal String
+print("1.This is normal String")
+
+
+# Doc String or Multiline String
+print("""2.This is a Doc String
+or Multiline String""")
+
+
+# f string
+f_string = "f"
+print(f"3.This is a {f_string} String")
+
+
+
+
+
+
+
+
+
+
+# ----------------------------Numbers----------------------------
+
+num_1 = 10 # int
+num_2 = 20.0 # float
+
+print(f"Type of num_1 is {type(num_1)} and type of num_2 is {type(num_2)}")
+print(f"The sum of {num_1} and {num_2} is {num_1 + num_2}")
+
+
+
+
+
+
+
+
+# ----------------------------Boolean----------------------------
+
+is_true = True
+is_false = False
+
+print(f"Type of is_true is {type(is_true)} and type of is_false is {type(is_false)}")
+
+
+
+
+
+
+
+# ----------------------------Variables----------------------------
+
+name = "Roman"
+roll_num = 12345
+roll_num_2 = 12345.70
+
+
+print(f"Student name is {name} and roll number is {roll_num}")
+print(f"""student name is {name}
+& student rool number is {roll_num}""")
+
+print(type(name))
+print(type(roll_num))
+print(type(roll_num_2))
