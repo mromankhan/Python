e881112b53a99f565e54cea09af6e3339dc2daac
Muhammad Roman <mromankhan005@gmail.com>
2025-04-16 17:07:23 +0500

    Lists & Tuples

---
+++ Class_3/app.py
@@ -0,0 +1,127 @@
+#                ---------------Task_1---------------
+
+# fav_fruits = []
+ 
+# hard way
+
+# f1 = int(input("Enter your Favourite fruit here: "))
+# fav_fruits.append(f1)
+
+# f2 = int(input("Enter your Favourite fruit here: "))
+# fav_fruits.append(f2)
+
+# f3 = int(input("Enter your Favourite fruit here: "))
+# fav_fruits.append(f3)
+
+# f4 = int(input("Enter your Favourite fruit here: "))
+# fav_fruits.append(f4)
+
+# f5 = int(input("Enter your Favourite fruit here: "))
+# fav_fruits.append(f5)
+
+# f6 = int(input("Enter your Favourite fruit here: "))
+# fav_fruits.append(f6)
+
+# f7 = int(input("Enter your Favourite fruit here: "))
+# fav_fruits.append(f7)
+
+# print(fav_fruits)
+
+
+
+
+# easy way
+
+# for i in range(7):
+#     fruit = int(input("Enter your Marks here: "))
+#     fav_fruits.append(fruit)
+
+# print(fav_fruits)
+
+
+
+
+
+
+
+
+
+
+#                ---------------Task_2---------------
+
+# marks = []
+ 
+# # hard way
+
+# m1 = int(input("Enter your Marks here: "))
+# marks.append(m1)
+
+# m2 = int(input("Enter your Marks here: "))
+# marks.append(m2)
+
+# m3 = int(input("Enter your Marks here: "))
+# marks.append(m3)
+
+# m4 = int(input("Enter your Marks here: "))
+# marks.append(m4)
+
+# m5 = int(input("Enter your Marks here: "))
+# marks.append(m5)
+
+# m6 = int(input("Enter your Marks here: "))
+# marks.append(m6)
+
+# m7 = int(input("Enter your Marks here: "))
+# marks.append(m7)
+
+# marks.sort()
+
+# print(marks)
+
+
+# easy way
+
+# for i in range(7):
+#     makrs = int(input("Enter your marks here: "))
+#     marks.append(makrs)
+    
+# marks.sort()
+# print(marks)
+
+
+
+
+
+
+
+#                ---------------Task_3---------------
+
+# t = (1, 2, 3, 4, 5)
+# t.append(6)
+# print(t)
+
+
+
+
+
+
+
+
+
+#                ---------------Task_4---------------
+
+# l = [1, 2, 3, 4, 5]
+# total = sum(l)
+# print(total)
+
+
+
+
+
+
+
+
+#                ---------------Task_5---------------
+
+# t = [0, 1, 2, 3, 4, 5, 0, 0]
+# print(t.count(0))
