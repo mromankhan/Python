6681226930eeca416f10a9ca1dcd83f3fa20f939
Muhammad Roman <mromankhan005@gmail.com>
2025-04-16 17:12:14 +0500

    Dictionary & Sets

---
+++ Class_4/app.py
@@ -0,0 +1,117 @@
+# ----------------------------------problem 1----------------------------------
+
+# words = {
+#     "madad": "Help",
+#     "kapray": "Clothes",
+#     "jutay": "Shoes",
+#     "ghari": "Watch",
+#     "chabi": "Key"
+# }
+
+# word = input("Enter a Word: ").lower()
+
+# print(words.get(word, "Word not found"))
+# print(words[word] if word in words else "Word not found")
+
+
+
+
+
+
+
+
+
+# ----------------------------------problem 2----------------------------------
+
+# numbers = set()
+
+# n1 = input("Enter Number 1: ")
+# numbers.add(int(n1))
+# n2 = input("Enter Number 2: ")
+# numbers.add(int(n2))
+# n3 = input("Enter Number 3: ")
+# numbers.add(int(n3))
+# n4 = input("Enter Number 4: ")
+# numbers.add(int(n4))
+# n5 = input("Enter Number 5: ")
+# numbers.add(int(n5))
+# n6 = input("Enter Number 6: ")
+# numbers.add(int(n6))
+# n7 = input("Enter Number 7: ")
+# numbers.add(int(n7))
+
+# print(numbers)
+
+
+
+
+
+
+
+
+
+
+# s = (18, "18")
+# print(s)
+
+
+
+
+
+
+
+
+
+
+
+# ----------------------------------problem 3----------------------------------
+
+# s = set()
+# s.add(20)
+# s.add(20.0)
+# s.add("20")
+
+# print(len(s))
+
+# problem 4
+
+# s = {}
+# print(type(s))
+
+
+
+
+
+
+
+
+
+
+
+
+# ----------------------------------problem 5----------------------------------
+
+# d = {}
+
+# name = input("Enter friend name: ")
+# language = input("Enter friend language: ")
+# d.update({name: language})
+
+# name = input("Enter friend name: ")
+# language = input("Enter friend language: ")
+# d.update({name: language})
+
+# name = input("Enter friend name: ")
+# language = input("Enter friend language: ")
+# d.update({name: language})
+
+# name = input("Enter friend name: ")
+# language = input("Enter friend language: ")
+# d.update({name: language})
+
+# name = input("Enter friend name: ")
+# language = input("Enter friend language: ")
+# d.update({name: language})
+
+# print(d)
+
