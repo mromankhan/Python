74c3692f51ebd64a724c831ee6d1c9c6ac704071
Muhammad Roman <mromankhan005@gmail.com>
2025-04-21 23:49:56 +0500

    Loops

---
+++ Class_6/app.py
@@ -0,0 +1,193 @@
+# ------------------------------Task 1------------------------------
+
+# n = int(input("Enter a Number: "))
+
+# for i in range(1, 11):
+#     print(f"{n} x {i} = {n * i}")
+
+
+
+
+
+
+
+
+
+# ------------------------------Task 2------------------------------
+
+# names = ["Alice", "Bob", "David", "Even", "shoib"]
+
+# for name in names:
+#     if(name.startswith("A")):
+#         print(f"Hello {name}")
+
+
+
+
+
+
+
+
+
+# ------------------------------Task 3------------------------------
+
+# n = int(input("Enter a Number: "))
+
+# i = 1
+
+# while(i <= 10):
+#     print(f"{n} x {i} = {n * i}")
+#     i += 1
+
+
+
+
+
+
+
+
+
+
+# ------------------------------Task 4------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ------------------------------Task 5------------------------------
+
+# n = int(input("Enter a Number: "))
+
+# i = 1
+# sum = 0
+# while(i <= n):
+#     sum += i
+#     i += 1
+
+# print(sum)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ------------------------------Task 6------------------------------
+
+# n = int(input("Enter a Number: "))
+
+# product = 1
+
+# for i in range(1, n + 1):
+#     product *= i
+
+# print(f"The Factorial of {n} is {product}")
+
+
+
+
+
+
+
+
+
+
+# ------------------------------Task 7------------------------------
+
+# n = int(input("Enter a Number: "))
+
+# for i in range(1, n + 1):
+#     print(" " * (n - i), end="")
+#     print("*" * (2 * i - 1), end="")
+#     print("")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ------------------------------Task 8------------------------------
+
+# n = int(input("Enter a Number: "))
+
+# for i in range(1, n + 1):
+#     print("*" * i, end="")
+#     print("")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ------------------------------Task 9------------------------------
+
+# n = int(input("Enter a Number: "))
+
+# for i in range(1, n + 1):
+#     if(i == 1 or i == n):
+#         print("*" * n, end="")
+#     else:
+#         print("*", end="")
+#         print(" " * (n - 2), end="")
+#         print("*", end="")
+#     print("")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ------------------------------Task 10------------------------------
+
+# n = int(input("Enter a Number: "))
+
+# for i in range(1, 11):
+#     print(f"{n} x {11 - i} = {n * (11 - i)}")
